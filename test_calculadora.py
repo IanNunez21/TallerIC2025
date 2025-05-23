eec393109cc0deaaa2a43b501ca31846edf5d7af
Ian Nuñez <138824368+IanNunez21@users.noreply.github.com>
2025-05-23 13:03:32 -0300

    Error de typeo arreglado, prueba erronea agregada para el workflow

--- test_calculadora.py
+++ test_calculadora.py
@@ -5,7 +5,8 @@ def test_suma():
     assert suma(2, 3) == 5
 
 def test_resta():
-    assert resta(5, 3) == 2
+    assert resta(5, 3) == 2  
+    assert resta(5, 4) == 0         #Error hecho a proposito
 
 def test_multiplicacion():
     assert multiplicacion(4, 3) == 12
