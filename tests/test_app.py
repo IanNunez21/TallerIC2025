62d382b9db70215c16d744c19d1949895984d016
Ian Nuñez <138824368+IanNunez21@users.noreply.github.com>
2025-06-05 13:20:15 -0300

    arreglado creo

--- tests/test_app.py
+++ tests/test_app.py
@@ -7,6 +7,11 @@ def client():
     with app.test_client() as c:
         yield c
 
+def test_get_index(client):
+    resp = client.get("/")
+    assert resp.status_code == 200
+    assert b"Calculadora" in resp.data
+
 def test_post_suma(client):
     resp = client.post("/", data={"a": "2", "op": "+", "b": "3"})
     assert b"Resultado: 5.0" in resp.data
